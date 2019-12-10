adad028e806840234ebae4f2dfe7c55b1ada6cc2
pratikw1859 <pratikw1859@gmail.com>
2019-12-10 18:13:46 +0530

    First Commit

---
+++ src/main/java/com/app/email/SpringBootEmailService3WithAttachmentApplication.java
@@ -0,0 +1,13 @@
+package com.app.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmailService3WithAttachmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmailService3WithAttachmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/email/SpringBootEmailService3WithAttachmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmailService3WithAttachmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
